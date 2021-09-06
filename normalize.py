f62b31ed8637ca5eee59dd8fc7b2f60b64128b58
YifanShenSZ <yshen57@jhu.edu>
2021-09-06 11:40:48 -0400

    merge normalize.py

--- normalize.py
+++ normalize.py
@@ -17,20 +17,12 @@ def parse_args() -> argparse.Namespace: # Command line input
 # Read a vector from file
 def read_vector(file:Path) -> numpy.ndarray:
     with open(file,'r') as f: lines = f.readlines()
-<<<<<<< HEAD
     v = []
     for line in lines:
         strs = line.split()
         for string in strs:
             v.append(float(string))
     return numpy.array(v)
-=======
-    l = []
-    for line in lines:
-        strs = line.split()
-        for str in strs: l.append(float(str))
-    return numpy.array(l)
->>>>>>> fa523aad426a1471bb5c2c9a2c7311720c2e916c
 # Write a vector to file (1 number/line)
 def write_vector(file:Path, vector:List) -> numpy.ndarray:
     with open(file,'w') as f:
