7d328d46ba40dd7bcc74a9077d501d77983fca5c
YifanShenSZ <yshen57@jhu.edu>
2021-11-02 13:30:37 -0400

    add a lot of cpp tools

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-for directory in cart2int cart2SASIC; do
+cpp_tools=(translate_IntCoordDef cart2int int2cart cart2SASIC)
+
+# build c++ tools
+cd cpp
+for cpp_tool in "${cpp_tools[@]}"; do
     echo
-    echo "Entre "$directory
-    cd $directory
+    echo "Entre "$cpp_tool
+    cd $cpp_tool
     # build
     if [ -d build ]; then rm -r build; fi
     mkdir build
@@ -9,9 +13,22 @@ for directory in cart2int cart2SASIC; do
     cmake -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_Fortran_COMPILER=ifort ..
     cmake --build .
     cd ..
-    # link exe
-    if [ -f $directory.exe ]; then rm $directory.exe; fi
-    ln -s build/$directory.exe
     # finish
     cd ..
 done
+cd ..
+
+# link every executable to bin/
+if [ -d bin ]; then rm -r bin; fi
+mkdir bin
+cd bin
+# link c++ tools
+for cpp_tool in "${cpp_tools[@]}"; do
+    ln -s ../cpp/$cpp_tool/build/$cpp_tool.exe
+done
+# link python tools
+for python_tool in ../python/*.py; do
+    ln -s $python_tool
+done
+# finish
+cd ..
--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,12 @@
-for directory in cart2int cart2SASIC; do
+cpp_tools=(translate_IntCoordDef cart2int int2cart cart2SASIC)
+
+# rebuild c++ tools
+cd cpp
+for cpp_tool in "${cpp_tools[@]}"; do
     echo
-    echo "Entre "$directory
-    cd $directory/build
+    echo "Entre "$cpp_tool
+    cd $cpp_tool/build
     cmake --build .
     cd ../..
 done
+cd ..
